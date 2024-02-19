efe8f65b58e64312342ae5b6864a5bdcdbc38013
Patch4Code <moritz-neumeier@outlook.de>
2024-02-19 16:25:30 +0100

    DiaryVElementEdit dialog added

--- app/src/main/java/com/moritz/movieappuitest/views/diary/DiaryEditElementView.kt
+++ app/src/main/java/com/moritz/movieappuitest/views/diary/DiaryEditElementView.kt
@@ -1,5 +1,7 @@
 package com.moritz.movieappuitest.views.diary
 
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,6 +12,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,8 +21,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -32,6 +39,9 @@ import java.net.URLDecoder
 @Composable
 fun DiaryEditElementView(navController: NavController, navViewModel: NavigationViewModel, loggedElement: String?){
 
+    val context = LocalContext.current
+    val openDiscardDialog = remember { mutableStateOf(false)  }
+
     LaunchedEffect(Unit) {
         navViewModel.updateScreen(Screen.DiaryEditElementScreen)
     }
@@ -42,6 +52,12 @@ fun DiaryEditElementView(navController: NavController, navViewModel: NavigationV
     val movieTitle = loggedElementData.movie.title
     val movieYear = MovieHelper.extractYear(loggedElementData.movie.releaseDate)
     val moviePosterUrl = MovieHelper.processPosterUrl(loggedElementData.movie.posterUrl)
+
+
+    BackHandler {
+        openDiscardDialog.value = true
+    }
+
 
     Column(modifier = Modifier.padding(16.dp)) {
         Row (modifier = Modifier
@@ -56,12 +72,34 @@ fun DiaryEditElementView(navController: NavController, navViewModel: NavigationV
                 Text(text = movieYear, color = Color.White, style = MaterialTheme.typography.bodyMedium)
             }
         }
+
+        if (openDiscardDialog.value) {
+            AlertDialog(
+                onDismissRequest = { openDiscardDialog.value = false },
+                title = {Text(text = "Discard Changes?")},
+                text = { Text(text = "") },
+                confirmButton = {
+                    Button(onClick = { navController.navigate(Screen.DiaryScreen.route) }) {
+                        Text(text = "Discard")
+                    }
+                },
+                dismissButton = {
+                    Button(onClick = { openDiscardDialog.value = false }) {
+                        Text(text = "Cancel")
+                    }
+
+                }
+            )
+        }
+
         Spacer(modifier = Modifier.padding(16.dp))
         Text(text = "Edit Rating", style = MaterialTheme.typography.titleMedium)
 
         Spacer(modifier = Modifier.padding(32.dp))
 
         Text(text = "Edit Date", style = MaterialTheme.typography.titleMedium)
+
+
 
         Column(verticalArrangement = Arrangement.Bottom, modifier = Modifier.weight(1f)){
             Divider(color = Color.DarkGray, thickness = 1.dp, modifier = Modifier
@@ -75,15 +113,21 @@ fun DiaryEditElementView(navController: NavController, navViewModel: NavigationV
                 ){
                 IconButton(
                     onClick = { /*TODO*/
-                        //Save Changes and navigate back to DiaryView
+                        //Update Data with Changes
+
+                        //navigate back to DiaryView
+                        navController.navigate(Screen.DiaryScreen.route)
+                        Toast.makeText(context, "Diary Entry updated", Toast.LENGTH_LONG).show()
+
                     },
 
                     ) {
                     Icon(imageVector = Icons.Default.Check, contentDescription = "Save Changes")
                 }
                 IconButton(
-                    onClick = { /*TODO*/
+                    onClick = {
                         //Popup Window discard changes or cancel
+                        openDiscardDialog.value = true
                     },
 
                     ) {
