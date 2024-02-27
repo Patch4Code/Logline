4c1c8bc3cf894a0d57ae41bef7e20e83910930c5
Patch4Code <moritz-neumeier@outlook.de>
2024-02-27 15:31:38 +0100

    ListsView delete by swipe viewModel work in progress

--- app/src/main/java/com/moritz/movieappuitest/views/list/ListsView.kt
+++ app/src/main/java/com/moritz/movieappuitest/views/list/ListsView.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -15,17 +16,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.moritz.movieappuitest.Screen
 import com.moritz.movieappuitest.dataclasses.MovieList
-import com.moritz.movieappuitest.dataclasses.userMovieListsDummy
+import com.moritz.movieappuitest.viewmodels.ListsViewModel
 import com.moritz.movieappuitest.viewmodels.NavigationViewModel
 import com.moritz.movieappuitest.views.list.item.ListsItem
 import com.moritz.movieappuitest.views.swipe.swipeToDeleteContainer
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter", "UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ListsView(navController: NavController, navViewModel: NavigationViewModel){
+fun ListsView(navController: NavController, navViewModel: NavigationViewModel, listsViewModel: ListsViewModel = viewModel()){
 
     LaunchedEffect(Unit) {
         navViewModel.updateScreen(Screen.ListsScreen)
@@ -40,34 +42,49 @@ fun ListsView(navController: NavController, navViewModel: NavigationViewModel){
             }
         },
         content = {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                content = {
-                    userMovieListsDummy.forEach{ list ->
-                        item {
-                            swipeToDeleteContainer(
-                                item = list,
-                                onDelete = {
-                                    //delete list (here only dummy lists)
-                                    val listToRemove = userMovieListsDummy.find { it.name == list.name }
-                                    listToRemove?.let { userMovieListsDummy.remove(it) }
-
-                                }
-                            ){
-                                ListsItem(navController, list)
-                            }
+            LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+                //val copyOfLists = userMovieListsDummy.toList()  // Kopie der Liste erstellen
+                itemsIndexed(
+                    items = listsViewModel.userMovieLists,
+                    key = { _, item -> item.hashCode() }
+                ) { _, list ->
+                    swipeToDeleteContainer(
+                        item = list,
+                        onDelete = {
+                            //delete list (here only dummy lists)
+                            listsViewModel.removeUserMovieList(list)
                         }
+                    ) {
+                        ListsItem(navController, list)
                     }
                 }
-            )
+
+
+            /*
+                userMovieListsDummy.forEachIndexed{ index, list ->
+                    item (key = index){
+                        swipeToDeleteContainer(
+                            item = list,
+                            onDelete = {
+                                //delete list (here only dummy lists)
+                                val listToRemove = userMovieListsDummy.find { it.name == list.name }
+                                listToRemove?.let { userMovieListsDummy.remove(it) }
+
+                            }
+                        ){
+                            ListsItem(navController, list)
+                        }
+                    }
+                }*/
+            }
             AddListDialog(
                 openAddListDialog = openAddListDialog.value,
                 onSave = { listName, isPublic ->
                     openAddListDialog.value = false
                     //add new List (here only to dummy list)
-                    userMovieListsDummy.add( MovieList(listName, isPublic,  mutableListOf() ))
+                    listsViewModel.addUserMovieList(MovieList(listName, isPublic, mutableListOf()))
+
+                    //userMovieListsDummy.add( MovieList(listName, isPublic,  mutableListOf() ))
                 },
                 onCancel = {openAddListDialog.value = false}
             )
