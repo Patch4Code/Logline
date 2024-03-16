27dd0c5fbcdc14ae085f9c01929860292e4a85d3
Patch4Code <moritz-neumeier@outlook.de>
2024-03-16 01:52:00 +0100

    general cleanup

--- app/src/androidTest/java/com/patch4code/loglinemovieapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/patch4code/loglinemovieapp/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.moritz.movieappuitest", appContext.packageName)
+        assertEquals("com.patch4code.loglinemovieapp", appContext.packageName)
     }
 }
