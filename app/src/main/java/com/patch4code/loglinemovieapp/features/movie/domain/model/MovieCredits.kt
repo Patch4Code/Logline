919d20e9082570dbb834ae9c867a6832b4516170
Patch4Code <moritz-neumeier@outlook.de>
2024-03-19 14:12:54 +0100

    Cast Crew Elements attempt with spaces (not working)

--- app/src/main/java/com/patch4code/loglinemovieapp/features/movie/domain/model/MovieCredits.kt
+++ app/src/main/java/com/patch4code/loglinemovieapp/features/movie/domain/model/MovieCredits.kt
@@ -9,30 +9,17 @@ data class MovieCredits(
 )
 
 data class Cast(
-    @SerializedName("adult") val adult: Boolean = false,
-    @SerializedName("gender") val gender: Int = 0,
     @SerializedName("id") val id: Int = -1,
-    @SerializedName("known_for_department") val department: String = "",
     @SerializedName("name") val name: String = "",
-    @SerializedName("original_name") val originalName: String = "",
-    @SerializedName("popularity") val popularity: Double = 0.0,
     @SerializedName("profile_path") val profilePath: String = "",
     @SerializedName("cast_id") val castId: Int = -1,
-    @SerializedName("character") val character: String = "",
-    @SerializedName("credit_id") val creditId: String = "",
-    @SerializedName("order") val order: Int = 0,
+    @SerializedName("character") val character: String = ""
 )
 
 data class Crew(
-    @SerializedName("adult") val adult: Boolean = false,
-    @SerializedName("gender") val gender: Int  = 0,
     @SerializedName("id") val id: Int  = -1,
-    @SerializedName("known_for_department") val knownForDepartment: String = "",
     @SerializedName("name") val name: String = "",
-    @SerializedName("original_name") val originalName: String = "",
-    @SerializedName("popularity") val popularity: Double = 0.0,
     @SerializedName("profile_path") val profilePath: String = "",
     @SerializedName("credit_id") val creditId: String = "",
-    @SerializedName("department") val department: String = "",
     @SerializedName("job") val job: String  = "",
 )
