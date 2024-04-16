b81a2598e9ec807cf4ac20e71439aa1c9c188f4d
Patch4Code <moritz-neumeier@outlook.de>
2024-04-16 15:42:26 +0200

    PublicReviewDetailsViewModel async problem fixed

--- app/src/main/java/com/patch4code/loglinemovieapp/features/social/presentation/screen_public_reviews/PublicReviewsViewModel.kt
+++ app/src/main/java/com/patch4code/loglinemovieapp/features/social/presentation/screen_public_reviews/PublicReviewsViewModel.kt
@@ -14,6 +14,10 @@ import com.patch4code.loglinemovieapp.features.core.presentation.utils.JSONHelpe
 import com.patch4code.loglinemovieapp.features.movie_public_reviews.domain.model.LoglineReview
 import kotlinx.coroutines.launch
 import java.lang.reflect.Type
+import java.util.concurrent.CountDownLatch
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class PublicReviewsViewModel: ViewModel() {
 
@@ -23,67 +27,90 @@ class PublicReviewsViewModel: ViewModel() {
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> get() = _isLoading
 
-    fun getPublicReviews(){
+    fun getPublicReviews() {
         viewModelScope.launch {
-
             try {
                 _isLoading.value = true
 
-                val reviewsQuery = ParseQuery<ParseObject>("LoggedMovie")
-
-                reviewsQuery.findInBackground { publicReviews, e ->
-                    if (publicReviews != null){
-                        val publicLoglineReviews = mutableListOf<LoglineReview>()
-                        for (log in publicReviews){
-                            val objectId = log.objectId
-                            val content = log.getString("review") ?: ""
-
-                            val longDate = log.getNumber("date")?.toLong()
-                            val date = DateHelper.convertLongToLocalDateTime(longDate)
-
-                            val rating = log.getNumber("rating")?.toInt() ?: 0
-                            val user = log.getParseUser("user")
-                            val userId = user?.objectId ?: ""
-
-                            val movieJson = log.getString("movie")
-                            val type: Type = object : TypeToken<Movie>() {}.type
-                            val movie: Movie = JSONHelper.fromJsonWithType(movieJson, type)
-
-                            var avatarPath = ""
-                            var authorName = "Anonymous"
-
-                            val profileQuery = ParseQuery.getQuery<ParseObject>("UserProfile")
-                            profileQuery.whereEqualTo("isPublic", true)
-                            profileQuery.whereEqualTo("user", user)
-                            val profile = profileQuery.find().firstOrNull()
-                            if (profile != null){
-                                avatarPath = profile.getParseFile("profileImage")?.url ?: ""
-                                authorName = profile.getString("userName") ?: "Anonymous"
-                            }
-
-                            val review = LoglineReview(
-                                objectId = objectId,
-                                authorName = authorName,
-                                userId = userId,
-                                content = content,
-                                movie = movie,
-                                createdAt = date,
-                                avatarPath = avatarPath,
-                                rating = rating
-                            )
-                            publicLoglineReviews.add(review)
+                // Kotlin suspendCoroutine waits for the completion of the background operation
+                val publicReviews = suspendCoroutine<List<ParseObject>> { continuation ->
+                    val reviewsQuery = ParseQuery<ParseObject>("LoggedMovie")
+                    // Execute query in background
+                    reviewsQuery.findInBackground { publicReviews, e ->
+                        if (e != null) {
+                            Log.e("PublicReviewsViewModel", "Error: ", e)
+                            continuation.resumeWithException(e)
+                        } else {
+                            continuation.resume(publicReviews)
                         }
-                        _publicReviews.value = publicLoglineReviews
-                        _isLoading.value = false
-                    }else if(e != null){
-                        Log.e("PublicReviewsViewModel", "Error: ", e)
                     }
                 }
+                // use the received publicReviews to create List<LoglineReview>
+                val publicLoglineReviews = fetchPublicLoglineReviews(publicReviews)
 
-            }catch (e: Exception){
-                Log.e("PublicReviewsViewModel", "Catch Error: ", e)
+                // Set the value of publicReviews LiveData to the fetched List<LoglineReview> sorted by date
+                _publicReviews.value = publicLoglineReviews.sortedByDescending { it.createdAt }
+            } catch (e: Exception) {
+                Log.e("PublicReviewsViewModel", "Error: ", e)
+            } finally {
+                _isLoading.value = false
             }
         }
     }
 
+    // Creates List<LoglineReview> from given List<ParseObject>
+    private suspend fun fetchPublicLoglineReviews(publicReviews: List<ParseObject>): List<LoglineReview> {
+        return suspendCoroutine { continuation ->
+            val publicLoglineReviews = mutableListOf<LoglineReview>()
+            val countDownLatch = CountDownLatch(publicReviews.size)
+
+            publicReviews.forEach { log ->
+                val objectId = log.objectId
+                val content = log.getString("review") ?: ""
+                val longDate = log.getNumber("date")?.toLong()
+                val date = DateHelper.convertLongToLocalDateTime(longDate)
+                val rating = log.getNumber("rating")?.toInt() ?: 0
+                val user = log.getParseUser("user")
+                val userId = user?.objectId ?: ""
+                val movieJson = log.getString("movie")
+                val type: Type = object : TypeToken<Movie>() {}.type
+                val movie: Movie = JSONHelper.fromJsonWithType(movieJson, type)
+
+                var avatarPath = ""
+                var authorName = "Anonymous"
+
+                val profileQuery = ParseQuery.getQuery<ParseObject>("UserProfile")
+                profileQuery.whereEqualTo("isPublic", true)
+                profileQuery.whereEqualTo("user", user)
+                profileQuery.findInBackground { profiles, e ->
+                    if (e != null) {
+                        Log.e("PublicReviewsViewModel", "Error loading user info: ", e)
+                    } else {
+                        val profile = profiles.firstOrNull()
+                        if (profile != null) {
+                            avatarPath = profile.getParseFile("profileImage")?.url ?: ""
+                            authorName = profile.getString("userName") ?: "Anonymous"
+                        }
+                    }
+
+                    val review = LoglineReview(
+                        objectId = objectId,
+                        authorName = authorName,
+                        userId = userId,
+                        content = content,
+                        movie = movie,
+                        createdAt = date,
+                        avatarPath = avatarPath,
+                        rating = rating
+                    )
+                    publicLoglineReviews.add(review)
+
+                    countDownLatch.countDown()
+                    if (countDownLatch.count == 0L) {
+                        continuation.resume(publicLoglineReviews)
+                    }
+                }
+            }
+        }
+    }
 }
