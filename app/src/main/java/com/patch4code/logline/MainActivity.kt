c3a44c1fc95835a39bb03580f4f6675a366ed537
Patch4Code <moritz-neumeier@outlook.de>
2025-01-25 15:54:09 +0100

    initialize db on start (fix for import problem)

--- app/src/main/java/com/patch4code/logline/MainActivity.kt
+++ app/src/main/java/com/patch4code/logline/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.patch4code.logline
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.patch4code.logline.features.navigation.presentation.screen_navigation.Navigation
 import com.patch4code.logline.room_database.LoglineDatabase
 import com.patch4code.logline.ui.theme.LoglineUiTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * GNU GENERAL PUBLIC LICENSE, VERSION 3.0 (https://www.gnu.org/licenses/gpl-3.0.html)
@@ -32,6 +37,17 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            try {
+                withContext(Dispatchers.IO) {
+                    db.runInTransaction { } // Empty transaction to initialize database
+                }
+            } catch (e: Exception) {
+                Log.e("DatabaseInit", "Error initializing database", e)
+            }
+        }
+
         setContent {
             // Applies the LoglineUiTheme to the entire activity
             LoglineUiTheme {
