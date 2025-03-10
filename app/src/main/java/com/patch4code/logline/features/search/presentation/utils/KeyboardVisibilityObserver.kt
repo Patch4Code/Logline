b410c9e956ab95eaab7a5a7b8e287cac4f252042
Patch4Code <116561421+Patch4Code@users.noreply.github.com>
2025-03-10 16:26:19 +0100

    split-screen search keyboard bug fixed

--- app/src/main/java/com/patch4code/logline/features/search/presentation/utils/KeyboardVisibilityObserver.kt
+++ app/src/main/java/com/patch4code/logline/features/search/presentation/utils/KeyboardVisibilityObserver.kt
@@ -1,8 +1,14 @@
 package com.patch4code.logline.features.search.presentation.utils
 
+import android.os.Handler
+import android.os.Looper
 import android.view.ViewTreeObserver
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -19,15 +25,24 @@ fun KeyboardVisibilityObserver(onKeyboardOpened:()->Unit = {}, onKeyboardClosed:
 
     val view = LocalView.current
     val viewTreeObserver = view.viewTreeObserver
+
+    var lastKeyboardState by remember { mutableStateOf(false) }
+
     DisposableEffect(viewTreeObserver) {
         val listener = ViewTreeObserver.OnGlobalLayoutListener {
             val isKeyboardOpen = ViewCompat.getRootWindowInsets(view)
                 ?.isVisible(WindowInsetsCompat.Type.ime()) ?: true
 
             if (isKeyboardOpen) {
+                lastKeyboardState = true
                 onKeyboardOpened()
-            } else {
-                onKeyboardClosed()
+            } else if (lastKeyboardState) {
+                lastKeyboardState = false
+
+                // Delay to prevent split screen keyboard problems
+                Handler(Looper.getMainLooper()).postDelayed({
+                    onKeyboardClosed()
+                }, 200)
             }
         }
         viewTreeObserver.addOnGlobalLayoutListener(listener)
