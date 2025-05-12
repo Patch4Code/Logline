b9281d3f1e30e64f7a7bcb312b7f90c2704d59bf
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-05-12 18:06:16 +0200

    enable edge to edge

--- app/src/main/java/com/patch4code/logline/MainActivity.kt
+++ app/src/main/java/com/patch4code/logline/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -48,6 +49,7 @@ class MainActivity : ComponentActivity() {
             }
         }
 
+        enableEdgeToEdge()
         setContent {
             // Applies the LoglineUiTheme to the entire activity
             LoglineUiTheme {
